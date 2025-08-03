5b8171eef1c08e14852343fb641a94777d34a5a9
mithunr03 <mithunachu80@gmail.com>
2025-08-03 12:18:07 +0530

    added //comments

--- CmToInces_InchesToCm.java
+++ CmToInces_InchesToCm.java
@@ -5,14 +5,14 @@ public class CmToInces_InchesToCm{
       Scanner sc =new Scanner(System.in);
       System.out.println("1.CONVERT CM TO INCHES");
       System.out.println("2.CONVERT INCHES TO CM");
-      System.out.print("Enter your choice: ");
+      System.out.print("Enter your choice: ");//getting the input from the user
       int choice = sc.nextInt();
-      if (choice == 1){
+      if (choice == 1){ //by using if condition we are checking the user input
           System.out.print("Enter length in centimeters: ");
           double cm = sc.nextDouble();
           double inches = cm / 2.54;
           System.out.printf("%.2f cm is equal to %.2f inches%n", cm, inches);
-      } else if (choice == 2) {
+      } else if (choice == 2) { // checking the user input for second option
           System.out.print("Enter length in inches: ");
           double inches = sc.nextDouble();
           double cm = inches * 2.54;
